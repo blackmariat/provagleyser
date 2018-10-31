b6ef97ceb271a537fa57d284dff072c443416a99
Maria <marialvesag@gmail.com>
2018-10-31 17:42:16 -0300

    upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PROVA</title>
+    </head>
+    <body>
+        <form action="tipo.php" method="post" enctype="multipart/form-data">
+            <b> <font size='5'> Escolha sua foto de perfil: </font> <b>
+                    <br> <br>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="Enviar foto">
+</form>
+    </body>
+</html>
+
